885e1fa4217df0fa74ac70ce9db1d40b9f8eb676
Royal <royalsachaniiitm@gmail.com>
2025-11-01 15:23:11 +0530

    Fix import case for MonacoEditor component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -16,7 +16,7 @@ import EditorPanel from "../components/dashboard/EditorPanel";
 import FileTabs from "../components/editor/fileTabs";
 import FileTree from "../components/editor/fileTree";
 
-import MonacoEditor from "../components/editor/monacoEditor";
+import MonacoEditor from "../components/editor/MonacoEditor";
 
 
 export default function Dashboard() {
